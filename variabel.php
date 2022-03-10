cbfcd719c07682767d016f024560ce946cc27525
sobariyah maghfiroh <sobariyahmaghfiroh1001@gmail.com>
2022-03-10 18:39:45 +0700

    variabel PHP

---
+++ variabel.php
@@ -0,0 +1,11 @@
+<?php 
+//ini adalah variabel untuk melakukan operasi aritmatika
+$a=1;
+$b=2;
+$sum=$a+$b;
+$subt=$a-$b;
+$mult=$a*$b;
+echo "Hasil penjumlahan dari : $a + $b = $sum <br>";
+echo "Hasil pengurangan dari : $a - $b = $subt <br>";
+echo "Hasil perkalian dari : $a x $b = $mult";
+ ?>
